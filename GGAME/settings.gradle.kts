6b9f8f8511e5361b57189cb3dd1d915603ec1e54
Mario <forinzffomo@gmail.com>
2025-08-28 21:45:08 +0200

    Some process

---
+++ GGAME/settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("java") {
+            from(files("gradle/java.versions.toml"))
+        }
+    }
+}
+
+rootProject.name = "GGAME"
+include(":app")
